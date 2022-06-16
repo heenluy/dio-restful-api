2136fd4780d2c35624a3d7f270e3147de1cf5093
Henrique Luiz <heenluy@gmail.com>
2022-06-16 15:45:55 -0300

    Initial commit

---
+++ src/main/java/me/heenluy/dio_restful/DioRestfulApplication.java
@@ -0,0 +1,13 @@
+package me.heenluy.dio_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/heenluy/dio_restful/DioRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package me.heenluy.dio_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
